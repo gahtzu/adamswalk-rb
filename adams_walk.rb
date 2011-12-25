23c32313b74298780ac9b9b706c8b3ee64a5c976
Jason Diehl <gahtzu@gmail.com>
2011-12-25 00:18:47 -0500

    battling? working

--- adams_walk.rb
+++ adams_walk.rb
@@ -2,7 +2,8 @@ module AdamsWalk
   class BattleError < StandardError
   end
 
-  def self.battle_over?(boss_health, adam_health)
-    boss_health > 0 || adam_health > 0
+  def self.battling?(boss_health, adam_health)
+    raise BattleError if boss_health < 0 || adam_health < 0
+    boss_health > 0 && adam_health > 0
   end
 end
--- adams_walk_spec.rb
+++ adams_walk_spec.rb
@@ -1,42 +1,42 @@
 require 'adams_walk'
 require 'rspec'
 
-describe "AdamsWalk#battle_over?" do
-  it "is false when both characters are over 0 health" do
-    AdamsWalk.battle_over?(5, 8).should == false
+describe "AdamsWalk#battling?" do
+  it "is true when both characters are over 0 health" do
+    AdamsWalk.battling?(5, 8).should == true
   end
 
-  it "is false when both characters are at 1 health" do
-    AdamsWalk.battle_over?(1, 1).should == false
+  it "is true when both characters are at 1 health" do
+    AdamsWalk.battling?(1, 1).should == true
   end
 
-  it "is true when both characters are at 0 health" do
-    AdamsWalk.battle_over?(0, 0).should == true
+  it "is false when both characters are at 0 health" do
+    AdamsWalk.battling?(0, 0).should == false
   end
 
-  it "is true when Boss is at 0 health" do
-    AdamsWalk.battle_over?(0, 4).should == true
+  it "is false when Boss is at 0 health" do
+    AdamsWalk.battling?(0, 4).should == false
   end
 
-  it "is true when Adam is at 0 health" do
-    AdamsWalk.battle_over?(4, 0).should == true
+  it "is false when Adam is at 0 health" do
+    AdamsWalk.battling?(4, 0).should == false
   end
 
   it "is raises an error when both characters are below 0 health" do
     lambda do
-      AdamsWalk.battle_over?(-4, -8)
+      AdamsWalk.battling?(-4, -8)
     end.should raise_error(AdamsWalk::BattleError)
   end
   
   it "is raises an error when Boss is below 0 health" do
     lambda do
-      AdamsWalk.battle_over?(-4, 8)
+      AdamsWalk.battling?(-4, 8)
     end.should raise_error(AdamsWalk::BattleError)
   end
 
   it "is raises an error when Adam is below 0 health" do
     lambda do
-      AdamsWalk.battle_over?(4, -8)
+      AdamsWalk.battling?(4, -8)
     end.should raise_error(AdamsWalk::BattleError)
   end
 end
